a3055dfb2a3151cb932399fb729f2eb37bcffb52
Aditya Suhas Iparkar <52993674+adityaiparkar@users.noreply.github.com>
2019-08-06 17:56:27 +0530

    Add files via upload
    Bug Report Form by using Bootstrap.

---
+++ srcBugReportForm/app/bug-info/bug-info.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bug-info',
+  templateUrl: './bug-info.component.html',
+  styleUrls: ['./bug-info.component.css']
+})
+export class BugInfoComponent{
+
+ 
+}

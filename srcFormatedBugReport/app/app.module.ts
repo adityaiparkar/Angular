d3619e3f61cf5ef2f07225d667edb55c8c72054d
Aditya Suhas Iparkar <52993674+adityaiparkar@users.noreply.github.com>
2019-08-10 13:21:37 +0530

    Add files via upload
    src of Formatted Bug Report.

---
+++ srcFormatedBugReport/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { BugInfoComponent } from './bug-info/bug-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PersonalInfoComponent,
+    BugInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AlertModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
